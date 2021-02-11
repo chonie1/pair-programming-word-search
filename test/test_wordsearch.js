e572075e13e0a5c9fa6cf1b22ada8aaa789832c1
Connie Ho <connie3@ualberta.ca>
2021-02-11 01:55:16 +0000

    finishing up word search

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
@@ -15,9 +15,25 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
+  });
+  -
+  it("should return true if the word is present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SEINFELD');
+
+    assert.isTrue(result);
   });
 
   it("should return true if the word is present", function() {
@@ -31,8 +47,63 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'LARRY');
 
     assert.isTrue(result);
   });
+
+  it("should return false if an empty array", function() {
+    const result = wordSearch([], 'LARRY');
+
+    assert.isFalse(result);
+  });
+
+  it("should return true if the word is reversed", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'WEST');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is diagonal", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'WIFE');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is diagonal reversed", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'EDTE');
+
+    assert.isTrue(result);
+  });
+
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,72 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function(matrix) {
+  const matrixHeight = matrix.length;
+  const matrixLength = matrix[0].length;
+  const result = [];
+  
+  for (let i = 0; i < matrixLength; i++) {
+    let matrixRow = [];
+    for (let j = 0; j < matrixHeight; j++) {
+      matrixRow.push(matrix[j][i]);
     }
-}
+    result.push(matrixRow);
+  }
+  return result;
+};
 
-module.exports = wordSearch
+const transform = function(matrix) {
+  const row = matrix.length;
+  const column = matrix[0].length;
+
+  const res = {};
+
+  for (let i = 0; i < row; i ++)  {
+    for (let j = 0; j < column; j ++) {
+      if (!res.hasOwnProperty(i - j)) {
+        res[i - j] = [matrix[i][j]];
+      } else {
+        res[i - j].push(matrix[i][j]);
+      }
+    }
+  }
+
+  return res;
+};
+
+const wordSearch = (letters, word) => {
+
+  if (!letters.length) {
+    return false;
+  }
+
+  const reverseWord = word.split('').reverse().join('');
+
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (const l of horizontalJoin) {
+    if (l.includes(word) || l.includes(reverseWord)) return true;
+  }
+    
+  const verticalJoin = transpose(letters).map(ls=>ls.join(''));
+  for (const v of verticalJoin) {
+    if ((v.includes(word)) || (v.includes(reverseWord)))  return true;
+  }
+
+  const diagonalJoin = Object.values(transform(letters)).map(ls=>ls.join(''));
+  for (const d of diagonalJoin) {
+    if ((d.includes(word)) || (d.includes(reverseWord)))  return true;
+  }
+    
+  // reverse items in array
+  for (const arr of letters) {
+    arr.reverse();
+  }
+
+  const reverseDiagonal = Object.values(transform(letters)).map(ls=>ls.join(''));
+  for (const r of reverseDiagonal) {
+    if ((r.includes(word)) || (r.includes(reverseWord)))  return true;
+  }
+
+  return false;
+};
+
+module.exports = wordSearch;
+
